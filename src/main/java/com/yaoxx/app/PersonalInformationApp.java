52e1932a9525b2a43320f9d2f2c73550ad2ce718
yaoxiaoxu <15203312301@163.com>
2019-05-07 17:11:19 +0800

    去除警告

--- src/main/java/com/yaoxx/app/PersonalInformationApp.java
+++ src/main/java/com/yaoxx/app/PersonalInformationApp.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.web.client.RestTemplate;
 
 /**
