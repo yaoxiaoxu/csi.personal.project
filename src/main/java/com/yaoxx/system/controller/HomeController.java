ca67b6cf764883cf6e7a38d0a9849ef34e15894d
yaoxiaoxu <15203312301@163.com>
2019-05-07 16:55:23 +0800

    測試 2

--- src/main/java/com/yaoxx/system/controller/HomeController.java
+++ src/main/java/com/yaoxx/system/controller/HomeController.java
@@ -6,15 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.yaoxx.business.permission.constant.PermissionType;
 import com.yaoxx.business.permission.service.PermissionService;
 import com.yaoxx.entity.Iuser;
 import com.yaoxx.entity.Permission;
 import com.yaoxx.system.iuser.service.IuserService;
-import com.yaoxx.test.TestService;
 
 @Controller
 public class HomeController {
